2c47e02ce1c68ab3ca3163ec67ddd72c384f3eb5
ghanesh <ghanesh.jain@gmail.com>
2020-04-10 13:39:12 +0530

    testfile

---
+++ training/src/test/java/com/selenium/training/Base.java
@@ -0,0 +1,8 @@
+package com.selenium.training;
+
+public class Base {
+	public static String URL="https://uat.mybbu.com";
+	public static String User="makeupartist";
+	public static String Pass="Srijan@123";
+
+}
